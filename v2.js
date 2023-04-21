94cda98405a85423954747d6adbf922a4029382d
ThomasViennet <viennet.t@gmail.com>
2023-04-21 17:46:15 +0200

    works with test data

--- v2.js
+++ v2.js
@@ -5,13 +5,15 @@
 const nodes = [
     ['A', ''],
     // ['Q', 'E'],
+    ['M', 'Y'],
     ['B', 'A'],
     ['C', 'A'],
     // ['D', 'Z'],
     ['E', 'D'],
     ['F', 'A'],
     // ['Z', ''],
-    ['Y', 'B']
+    ['Y', 'B'],
+    ['K','C'],
 ];
 
 function mindmeister(nodes) { //nodes = [['child', 'parent'], ...];
@@ -26,28 +28,28 @@ function mindmeister(nodes) { //nodes = [['child', 'parent'], ...];
         }
     });
 
-    console.log(response);
-
     // Find parent's children
     function findChildren(parent, nodes) {
         let children = nodes.filter(node => node[1] === parent[0]);
-        return children.map(child => [child[0]]);
+        return children.map(child => child[0]);
     }
 
     // Find children of each parent
     for (let parent = 0; parent < response.length; parent++) {// 
-        
-        let indent = [' '];
-        children = findChildren(response[parent], nodes);
+
         let rangParent = parent;
 
+        let indent = ' ';
+        children = findChildren(response[parent].filter(element => element !== indent), nodes);
+        let indentParent = response[parent].filter(element => element === indent);
+        indentParent.push(indent);
+
         for (child of children) {
-            response.splice(++rangParent, 0, child);//ajouter indentation pour enfant avec celle de son parent
-            console.log(response);
+            response.splice(++rangParent, 0, indentParent.concat(child));
         }
     }
-  
-    return response;
+
+    return response.map(node => [node.join('')]);
 }
 
 console.log(mindmeister(nodes));
