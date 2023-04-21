4d3787f984dd3bda06cbf56cac1438c91e8a3839
ThomasViennet <viennet.t@gmail.com>
2023-04-21 16:18:38 +0200

    clean code

--- v2.js
+++ v2.js
@@ -19,37 +19,32 @@ function mindmeister(nodes) { //nodes = [['child', 'parent'], ...];
     let response = [];
 
     // Find tree’s roots
-    let children = nodes.map(child => child[0]);// bad ?
+    let children = nodes.map(child => child[0]);
     nodes.forEach(nodes => {
         if (nodes[1] == '' || !children.includes(nodes[1])) {//no parent or parent unknown = root
             response.push([nodes[0]]);
         }
     });
-    console.log('1');
+
     console.log(response);
 
     // Find parent's children
     function findChildren(parent, nodes) {
         let children = nodes.filter(node => node[1] === parent[0]);
-        // return children.map(child => [indent + child[0]]);
         return children.map(child => [child[0]]);
     }
 
     // Find children of each parent
     for (let parent = 0; parent < response.length; parent++) {// 
         
-        let parentBackup = parent;
         let indent = [' '];
-
         children = findChildren(response[parent], nodes);
+        let rangParent = parent;
 
         for (child of children) {
-            response.splice(parent + 1, 0, child);//ajouter indentation pour enfant avec celle de son parent
-            parent++;
+            response.splice(++rangParent, 0, child);//ajouter indentation pour enfant avec celle de son parent
             console.log(response);
         }
-
-        parent = parentBackup;
     }
   
     return response;
