a27c67548ed8af116637c14e809cad5cbc0cf9b2
ThomasViennet <viennet.t@gmail.com>
2023-04-21 17:57:58 +0200

    V2 adapted to google sheet

---
+++ mindmeister-v2.js
@@ -0,0 +1,43 @@
+/*
+* Input : [['child', 'parent'], ...];
+* Reponse : [['parent'],[' child']]
+*/
+
+function mindmeister() {
+    const sheet = SpreadsheetApp.getActiveSpreadsheet();
+    const input = sheet.getSelection().getActiveRange().getValues();
+    const output = sheet.getSheetByName("Mindmeister - Export");
+    output.clear();
+
+    let response = [];
+
+    // Find tree’s roots
+    let children = input.map(child => child[0]);
+    input.forEach(input => {
+        if (input[1] == '' || !children.includes(input[1])) {//no parent or parent unknown = root
+            response.push([input[0]]);
+        }
+    });
+
+    // Find parent's children
+    function findChildren(parent, input) {
+        let children = input.filter(node => node[1] === parent[0]);
+        return children.map(child => child[0]);
+    }
+
+    // Find children of each parent
+    for (let parent = 0; parent < response.length; parent++) {// 
+
+        let rangParent = parent;
+
+        let indent = ' ';
+        children = findChildren(response[parent].filter(element => element !== indent), input);
+        let indentParent = response[parent].filter(element => element === indent);
+        indentParent.push(indent);
+
+        for (child of children) {
+            response.splice(++rangParent, 0, indentParent.concat(child));
+        }
+    }
+    output.getRange(1, 1, response.length, 1).setValues(response.map(node => [node.join('')]));
+}
